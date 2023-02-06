600be9768a194244a62e08c638f0cd52bc2feed4
secretcode <sandipermana88@gmail.com>
2023-02-06 11:53:22 +0700

    destructive

---
+++ destructive.js
@@ -0,0 +1,14 @@
+const employee = {
+    firstName: 'ilham',
+    lastName: 'hasyah',
+    origin: 'tangerang'
+}
+
+// var firstName = employee.firstName;
+// var lastName = employee.lastName;
+// var origin = employee.origin;
+
+// console.log(firstName);
+
+const { firstName, lastName, origin } = employee;
+console.log(firstName);
--- spread_operator.js
+++ spread_operator.js
@@ -1,6 +1,6 @@
 //spread operator
 var brand = ['honda', 'bmw', 'toyota', 'subaru']
-var otherBrand = [...brand, ]
+var otherBrand = [...brand, brand[2] ]
 var allBrand = [...brand, ...otherBrand]
 
 console.log(brand);
