06c4277e60dcad7fd3d3f381d4154f1392f6db0f
secretcode <sandipermana88@gmail.com>
2023-02-06 09:47:13 +0700

    2nd file added

---
+++ hello.js
@@ -0,0 +1,29 @@
+console.log("hai");
+
+let a = 10
+
+let b = 20
+
+console.log(a, b)
+
+
+const nama = 'sandi';
+
+
+console.log(nama, "saya adalah ", a)
+
+console.log(typeof(a));
+
+console.log(typeof(nama))
+
+const rumah = {
+  cat: "putih",
+  atap: "genteng",
+  lantai: "keramik",
+  jml_ruangan: 5,
+}
+
+console.log(rumah);
+
+const Hobby = ['futsal', 'badminton', 'renang']
+console.log(Array.isArray(rumah))
