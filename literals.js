c825f4fad77216f7f2da78f7537612d4b85a955b
secretcode <sandipermana88@gmail.com>
2023-02-06 11:40:26 +0700

    spread_operator

---
+++ literals.js
@@ -0,0 +1,13 @@
+//es5
+var nilai = '10';
+var Rapor = "Nilai anda adalah: " + nilai;
+console.log(Rapor);
+
+var jml_ruangan = 2;
+var luas_tanah = 100;
+var spesifikasi_rumah = `spesifikasi rumah ini:
+    1. mempunyai ${jml_ruangan} kamar tidur
+    2. luas tanah sebesar ${luas_tanah}`;
+
+console.log(spesifikasi_rumah);
+
---
+++ spread_operator.js
@@ -0,0 +1,8 @@
+//spread operator
+var brand = ['honda', 'bmw', 'toyota', 'subaru']
+var otherBrand = [...brand, ]
+var allBrand = [...brand, ...otherBrand]
+
+console.log(brand);
+console.log(otherBrand);
+console.log(allBrand);
